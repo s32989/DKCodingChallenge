afd5b87e9d0bd26f0ece22e7b67bd5c89f6668b5
s32989 <s32989@gmail.com>
2021-04-12 14:33:23 -0400

    unused imports

--- src/main/java/com/dkchallenge/springboot/Application.java
+++ src/main/java/com/dkchallenge/springboot/Application.java
@@ -3,8 +3,6 @@ package com.dkchallenge.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 
 @SpringBootApplication
 public class Application {
