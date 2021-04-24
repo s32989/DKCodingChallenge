e54ddde7c79206ceecb486c2eaa8ce7fcae46b5b
s32989 <s32989@gmail.com>
2021-04-24 16:23:20 -0400

    reformatted request, built helper method in data service

--- src/main/java/com/dkchallenge/springboot/controller/request/SearchRequest.java
+++ src/main/java/com/dkchallenge/springboot/controller/request/SearchRequest.java
@@ -2,15 +2,19 @@ package com.dkchallenge.springboot.controller.request;
 
 import java.util.HashMap;
 
-public abstract class SearchRequest {
+public class SearchRequest {
     private final int indexBegin;
     private final int indexEnd;
     private final int winLength;
+    private HashMap<String, Float> thresholds;
+    private HashMap<String, String> columns;
 
-    public SearchRequest(int indexBegin, int indexEnd, int winLength) {
+    public SearchRequest(int indexBegin, int indexEnd, int winLength, HashMap<String, Float> thresholds, HashMap<String, String> columns) {
         this.indexBegin = indexBegin;
         this.indexEnd = indexEnd;
         this.winLength = winLength;
+        this.thresholds = thresholds;
+        this.columns = columns;
     }
 
     public int getIndexBegin() {
@@ -23,5 +27,13 @@ public abstract class SearchRequest {
 
     public int getWinLength() {
         return winLength;
+    }
+
+    public HashMap<String, Float> getThresholds() {
+        return thresholds;
+    }
+
+    public HashMap<String, String> getColumns() {
+        return columns;
     }
 }
